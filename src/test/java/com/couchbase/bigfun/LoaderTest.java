5b6ed11a20e52f80c153c24b2c1f2e97a0f0002f
hui wang <hui.wang@couchbase.com>
2017-10-05 16:46:47 -0700

    Disable env specific tests.

--- src/test/java/com/couchbase/bigfun/LoaderTest.java
+++ src/test/java/com/couchbase/bigfun/LoaderTest.java
@@ -340,7 +340,7 @@ public class LoaderTest
     }
 
     // Env specific test, disabled by default remove "_" in method to enable
-    public void testLoadTarget()
+    public void _testLoadTarget()
     {
         TargetInfo targetinfo = new TargetInfo("172.23.98.29", "bucket-1", "bucket-1", "password", "172.23.98.30");
         LoadTarget target = new LoadTarget(targetinfo);
@@ -386,7 +386,7 @@ public class LoaderTest
     }
 
     // Env specific test, disabled by default remove "_" in method to enable
-    public void testLoadTargetRetry() {
+    public void _testLoadTargetRetry() {
         TargetInfo targetinfo = new TargetInfo("172.23.98.29", "bucket-1", "bucket-1", "password", "172.23.98.30");
         String key = "1";
         String docJson = "{\"id\" : \"1\", \"updatefieldname\" : \"2000-01-02\", \"field2\" : \"abcd\"}";
@@ -594,7 +594,7 @@ public class LoaderTest
     }
 
     // Env specific test, disabled by default remove "_" in method to enable
-    public void testLoader() {
+    public void _testLoader() {
         int updateNum = 10;
         int deleteNum = 11;
         int insertNum = 12;
@@ -660,7 +660,7 @@ public class LoaderTest
     }
 
     // Env specific test, disabled by default remove "_" in method to enable
-    public void testBatchModeLoader() {
+    public void _testBatchModeLoader() {
         String dataFile = "data.json";
         String metaFile = "data.meta";
         String docContents[] = initializeJsonDocs(20);
@@ -827,7 +827,7 @@ public class LoaderTest
     }
 
     // Env specific test, disabled by default remove "_" in method to enable
-    public void testMixModeLoader() {
+    public void _testMixModeLoader() {
         String dataFile = "data.json";
         String metaFile = "data.meta";
         String docContents[] = initializeJsonDocs(200);
